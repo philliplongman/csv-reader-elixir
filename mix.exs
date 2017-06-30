5d0491b1daedcabb915057480ee896ff79e658d9
Phillip Longman <himself@philliplongman.com>
2017-06-30 00:02:53 -0500

    Use slim for templates.

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Reader.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:phoenix_slime, "~> 0.8.0"}]
   end
 end
