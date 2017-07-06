9bab370d4911a0c8f80c701305a262e74048c0f0
Phillip Longman <himself@philliplongman.com>
2017-07-06 02:13:08 -0400

    Prepare for deployment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, "./web/static/js/app.js"),
@@ -6,6 +8,28 @@ module.exports = {
     path: path.resolve(__dirname, "./priv/static/js"),
     filename: "app.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: "babel-loader",
+          options: {
+            ignore: "/node_modules/",
+            presets: ["es2015"]
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   resolve: {
     alias: { "vue$": "vue/dist/vue.esm.js" }
   }
