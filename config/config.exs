bbec80dfd654397c3c12538c687c714f7f4eaa9a
Phillip Longman <himself@philliplongman.com>
2017-11-13 14:21:23 -0500

    Use eex for templates.

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,3 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
-
-config :phoenix, :template_engines,
-  slim: PhoenixSlime.Engine,
-  slime: PhoenixSlime.Engine
